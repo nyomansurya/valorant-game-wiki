a665f575e2c2556643e33bc8146a3f629510102f
nyomansurya <coksuryaaditya@gmail.com>
2023-12-15 14:03:09 +0700

    modified app logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_valorantagent/page/home_screen/home_screen.dart';
+import 'package:flutter_valorantagent/page/splash_screen.dart';
 
 void main() {
   runApp(const ValorantAgentApp());
@@ -12,7 +13,7 @@ class ValorantAgentApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
